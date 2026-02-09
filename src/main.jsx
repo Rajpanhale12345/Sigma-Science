4db75a3bd04ba209157af50c811b62350ad13f15
Raj <panhaleraj003@gmail.com>
2026-02-09 14:51:00 +0530

    merge conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-<<<<<<< HEAD
-import App from "./App";
-=======
 import { HelmetProvider } from "react-helmet-async";
-import App from "../App";
->>>>>>> c0b759a1c50cad137a69cc32a2f9b294c5430567
+import App from "./App";     
 import store from "../store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
